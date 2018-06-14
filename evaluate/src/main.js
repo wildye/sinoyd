fc74b15268475bfffd10687491cd40fb3b40d4f8
凝弧 <free.ye@live.cn>
2018-06-15 07:35:14 +0800

    evalute

--- evaluate/src/main.js
+++ evaluate/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-// 引用API文件
+// 引用文件
 import api from './api/index.js'
-// 将API方法绑定到全局
+import utils from './utils/index.js'
+// 将方法绑定到全局
 Vue.prototype.$api = api
+Vue.prototype.$utils = utils
 
 Vue.config.productionTip = false
 
